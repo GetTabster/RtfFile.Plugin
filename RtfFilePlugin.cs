4b35c3b0e9e2c1745719201e6ce9df8ee3e93c89
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-30 16:10:20 -0400

    Version->TabsterVerseion

--- RtfFilePlugin.cs
+++ RtfFilePlugin.cs
@@ -32,9 +32,9 @@ namespace RtfFile
             get { return "RTF support"; }
         }
 
-        public Version Version
+        public TabsterVersion Version
         {
-            get { return new Version("1.0"); }
+            get { return new TabsterVersion("1.0"); }
         }
 
         public Uri Website
